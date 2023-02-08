d78b1c03d77163840146adcd0a78ae5229e06ddb
thiagosilva977 <thiagosilva977@hotmail.com>
2023-02-08 10:32:43 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+from source.test_heni import HeniTrial
+
+if __name__ == '__main__':
+    scraper_class = HeniTrial()
+    scraper_class.start_test()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ source/test_heni.py
@@ -0,0 +1,13 @@
+
+class HeniTrial:
+    def __init__(self):
+        print('')
+
+    def start_test(self):
+
+
+        print('initializing')
+
+
+    def first_challenge(self):
+        print('first challenge')
